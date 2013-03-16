92da29ddbf0e8724d27d0b755dc582bf6faecc09
jake <bigjake2s@gmail.com>
2013-03-15 23:48:14 -0400

    Simple template management API established

--- template/template.go
+++ template/template.go
@@ -1,16 +1,42 @@
 package template
 
 import (
+	"html/template"
 	"io"
+	"time"
 )
+
+type manager struct {
+	Template     *template.Template
+	LastModified time.Time
+}
+
+var templates = map[string]*manager{}
 
 func WriteFooter(w io.Writer, ctx *Context) {
 	w.Write([]byte("FOOTER\n"))
 }
 
-func WriteHeader(w io.Writer, ctx *Context) {
+func WriteHeader(w io.Writer, ctx *Context) error {
 	w.Write([]byte("HEADER\n"))
-
+	return Write(w, ctx, "header", "header.html")
 }
 
-func verifyCurrent()
+func Write(w io.Writer, ctx *Context, name string, files ...string) error {
+	w.Write([]byte("WRITER\n"))
+	verifyCurrent(name, files...)
+	return templates[name].Template.Execute(w, ctx)
+}
+
+func verifyCurrent(name string, files ...string) (err error) {
+	t, ok := templates[name]
+	if !ok {
+		t = &manager{
+			Template: template.New(name),
+		}
+		templates[name] = t
+	}
+	//_, err = t.Template.ParseFiles(files...)
+	_, err = t.Template.Parse("<!DOCTYPE html><html><head><title>{{.Title}}</title></head><body>hi")
+	return
+}
