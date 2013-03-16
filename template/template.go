5f6c59b67a8fd54f933bacc1e4208a3fc696448d
jake <bigjake2s@gmail.com>
2013-03-16 10:17:13 -0400

    Changing behavior of templating layer

--- template/template.go
+++ template/template.go
@@ -1,22 +1,43 @@
 package template
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 )
 
-type manager struct {
-	Template     *template.Template
+const TemplateDir = filepath.Join("assets", "templates")
+
+type File struct {
+	Path         string
 	LastModified time.Time
+}
+
+type FileManager struct {
+	Files []File
+}
+
+func (f File) Modified() (modified bool, err error) {
+	fi, err := os.Stat(f.Path)
+	if err != nil {
+		return false, fmt.Errorf("template/File.Modified: %s", err)
+	}
+	if modTime := fi.ModTime(); !modTime.Equal(f.LastModified) {
+		f.LastModified = modTime
+		modified = true
+	}
+	return
 }
 
 var templates = map[string]*manager{}
 
-func WriteFooter(w http.ResponseWriter, ctx *Context) {
+func WriteFooter(w http.ResponseWriter, ctx *Context) error {
 	w.Write([]byte("FOOTER\n"))
+	return nil
 }
 
 func WriteHeader(w http.ResponseWriter, ctx *Context) error {
@@ -32,8 +53,10 @@ func Write(w http.ResponseWriter, ctx *Context, name string, files ...string) er
 }
 
 func verifyCurrent(name string, files ...string) (err error) {
+	log.Printf("Verifying %s %v", name, files)
 	t, ok := templates[name]
 	if !ok {
+		log.Print("Building new manager")
 		t = &manager{
 			Template: template.New(name),
 		}
@@ -41,21 +64,27 @@ func verifyCurrent(name string, files ...string) (err error) {
 	}
 	reparse := false
 	filenames := make([]string, len(files))
-	for _, file := range files {
+	for i, file := range files {
 		filename := filepath.Join("assets", "templates", file)
 		in, err := os.Stat(filename)
 		if err != nil {
+			log.Printf("Could not stat %s", filename)
 			return err
 		}
 		if mtime := in.ModTime(); t.LastModified.Before(mtime) {
+			log.Printf("Last modified [%s]: %s", filename, mtime)
 			reparse = true
 			t.LastModified = mtime
 		}
-		filenames = append(filenames, filename)
+		filenames[i] = filename
 	}
+	log.Printf("Filenames: %+q", filenames)
 	if reparse {
+		log.Print("Templates changed, reparsing")
 		_, err = t.Template.ParseFiles(filenames...)
-		//_, err = t.Template.Parse("<!DOCTYPE html><html><head><title>{{.Title}}</title><<body>hi")
+		if err != nil {
+			log.Printf("Error reparsing: %s", err)
+		}
 	}
 	return
 }
