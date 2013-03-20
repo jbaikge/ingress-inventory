b35db81275ca467ff40567afc5ebed38e182583e
jake <bigjake2s@gmail.com>
2013-03-20 11:24:30 -0400

    Added func to get the CurrentState; default to zero-state

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -3,3 +3,10 @@ package inventory
 type Inventory struct {
 	States []State
 }
+
+func (i Inventory) CurrentState() State {
+	if len(i.States) == 0 {
+		return NewState()
+	}
+	return i.States[len(i.States)-1]
+}
