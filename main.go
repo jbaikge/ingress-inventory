df3919fe2872e1ea353da1ff7ba461305025c719
jake <bigjake2s@gmail.com>
2013-03-20 11:23:53 -0400

    Re-enabled logging handler

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"github.com/gorilla/handlers"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"net/http"
 	"os"
@@ -12,8 +12,7 @@ var (
 )
 
 func main() {
-	//http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, router))
-	http.Handle("/", router)
+	http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, router))
 
 	panic(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
