8143d6b3c112bab2c92cdef7818d6e0c4098cd1f
jake <bigjake2s@gmail.com>
2013-04-06 08:54:06 -0400

    Added the 'trouble pages'

---
+++ page_trouble.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/jbaikge/ingress-inventory/parser"
+	"log"
+	"net/http"
+)
+
+func init() {
+	router.HandleFunc("/profileNotFound", HandleTrouble)
+}
+
+func HandleTrouble(w http.ResponseWriter, r *http.Request) {
+	ctx := &parser.Context{
+		Title:       "Uh-oh",
+		Description: "Something went wrong",
+	}
+	if err := parser.Render(w, ctx, "trouble.html"); err != nil {
+		log.Println(err)
+	}
+}
