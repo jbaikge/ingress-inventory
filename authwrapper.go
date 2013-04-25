78e38cebfea19c98bac708ca26524d4be1aedf4f
jake <bigjake2s@gmail.com>
2013-04-25 15:40:15 -0400

    Cleaning up, adding TODOs

--- authwrapper.go
+++ authwrapper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/jbaikge/ingress-inventory/parser"
 	"github.com/jbaikge/ingress-inventory/profile"
+	"labix.org/v2/mgo/bson"
 	"log"
 	"net/http"
 )
@@ -11,19 +12,17 @@ type AuthFunc func(http.ResponseWriter, *http.Request, *parser.Context)
 
 func AuthWrapper(f AuthFunc) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		p := profile.Profile{}
-		if cookie, err := r.Cookie("Profile"); err == nil {
-			
-			if err = sCookie.Decode(cookie.Name, cookie.Value, &p); err != nil {
+		ctx := &parser.Context{}
+
+		if cookie, err := r.Cookie("Id"); err == nil {
+			var id bson.ObjectId
+			if err = sCookie.Decode(cookie.Name, cookie.Value, &id); err != nil {
 				log.Print(err)
+				// TODO: redirect to a 500 page or clear cookies and bounce to /
 			}
+			// TODO: use id to fetch ctx.Profile
 		}
 
-		log.Printf("Username: %s", p.DisplayUsername)
-
-		ctx := &parser.Context{
-			Profile: p,
-		}
 		f(w, r, ctx)
 	}
 }
