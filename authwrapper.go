83557741a53f2efc678afbc2534dc788cc5a7afc
jake <bigjake2s@gmail.com>
2013-04-25 16:02:52 -0400

    Fixed my derp. AuthWrapper more idiomatic.
    Thanks @skelterjohn and @moraes!

--- authwrapper.go
+++ authwrapper.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/jbaikge/ingress-inventory/parser"
-	"github.com/jbaikge/ingress-inventory/profile"
 	"labix.org/v2/mgo/bson"
 	"log"
 	"net/http"
@@ -14,14 +13,19 @@ func AuthWrapper(f AuthFunc) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := &parser.Context{}
 
-		if cookie, err := r.Cookie("Id"); err == nil {
-			var id bson.ObjectId
-			if err = sCookie.Decode(cookie.Name, cookie.Value, &id); err != nil {
-				log.Print(err)
-				// TODO: redirect to a 500 page or clear cookies and bounce to /
-			}
-			// TODO: use id to fetch ctx.Profile
+		cookie, err := r.Cookie("Id")
+		if err != nil {
+			// "Named cookie not present"
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
 		}
+
+		var id bson.ObjectId
+		if err = sCookie.Decode(cookie.Name, cookie.Value, &id); err != nil {
+			log.Print(err)
+			// TODO: redirect to a 500 page or clear cookies and bounce to /
+		}
+		// TODO: use id to fetch ctx.Profile
 
 		f(w, r, ctx)
 	}
