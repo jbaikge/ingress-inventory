04d19e79f637859b29afdfdb3ecd4419aa638dec
jake <bigjake2s@gmail.com>
2013-04-26 14:18:36 -0400

    Slight URL update: inventory-add -> inventory/update; Adding inventory/save

--- page_inventoryadd.go
+++ page_inventoryadd.go
@@ -7,12 +7,21 @@ import (
 )
 
 func init() {
-	router.HandleFunc("/inventory-add", AuthWrapper(HandleInventoryAdd))
+	router.HandleFunc("/inventory/update", AuthWrapper(HandleInventoryUpdate))
+	router.HandleFunc("/inventory/save", AuthWrapper(HandleInventorySave))
 }
 
-func HandleInventoryAdd(w http.ResponseWriter, r *http.Request, ctx *parser.Context) {
+func HandleInventoryUpdate(w http.ResponseWriter, r *http.Request, ctx *parser.Context) {
 	ctx.Title = "Ingress Inventory"
 	if err := parser.Render(w, ctx, "update-inventory.html"); err != nil {
 		log.Println(err)
 	}
+}
+
+func HandleInventorySave(w http.ResponseWriter, r *http.Request, ctx *parser.Context) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/inventory", http.StatusTemporaryRedirect)
+		return
+	}
+	http.Redirect(w, r, "/inventory", http.StatusTemporaryRedirect)
 }
