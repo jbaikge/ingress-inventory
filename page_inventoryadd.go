aa0d073cbbed6c51c14f6570f62a778f59ee8f0c
jake <bigjake2s@gmail.com>
2013-04-28 08:46:51 -0400

    Built up the inventory/update page
    * Added custom parser for the entry time
    * Temporarily prefilled the inventory values for testing

--- page_inventoryadd.go
+++ page_inventoryadd.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"github.com/gorilla/schema"
+	"github.com/jbaikge/ingress-inventory/inventory"
 	"github.com/jbaikge/ingress-inventory/parser"
 	"log"
 	"net/http"
+	"reflect"
+	"time"
+)
+
+var (
+	inventoryDecoder *schema.Decoder
+	timeLayout       = time.RFC1123
 )
 
 func init() {
 	router.HandleFunc("/inventory/update", AuthWrapper(HandleInventoryUpdate))
 	router.HandleFunc("/inventory/save", AuthWrapper(HandleInventorySave))
+	inventoryDecoder = schema.NewDecoder()
+	inventoryDecoder.RegisterConverter(time.Time{}, convertDatetime)
 }
 
 func HandleInventoryUpdate(w http.ResponseWriter, r *http.Request, ctx *parser.Context) {
@@ -23,5 +34,25 @@ func HandleInventorySave(w http.ResponseWriter, r *http.Request, ctx *parser.Con
 		http.Redirect(w, r, "/inventory", http.StatusTemporaryRedirect)
 		return
 	}
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+	}
+	s := inventory.NewState()
+	// Fill the profile communities with all, filter out falses later
+	if err := inventoryDecoder.Decode(&s, r.PostForm); err != nil {
+		errs, _ := err.(schema.MultiError)
+		for f, e := range errs {
+			log.Printf("%14s: %s", f, e)
+		}
+	}
+	log.Printf("%+v", s)
 	http.Redirect(w, r, "/inventory", http.StatusTemporaryRedirect)
+}
+
+func convertDatetime(value string) reflect.Value {
+	if v, err := time.Parse(timeLayout, value); err == nil {
+		return reflect.ValueOf(v)
+	}
+	log.Printf("Could not parse as time: '%s' using '%s'", value, timeLayout)
+	return reflect.Value{}
 }
