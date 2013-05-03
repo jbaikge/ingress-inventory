cebb7669c1fd87f6d41c9e4b55d767fb987c6b46
jake <bigjake2s@gmail.com>
2013-05-02 22:03:17 -0400

    Improved time validation and conversion on frontend
    * Added JavaScript toRFC3339() to convert EntryTime to value for Time

--- assets/js/update-inventory.js
+++ assets/js/update-inventory.js
@@ -1,18 +1,37 @@
 $(function() {
+	$("#InventoryForm input[name=Timezone]").val()
 	$("#InventoryForm").submit(function() {
 		var self = $(this)
-		if (validateTime(self.find("input[name=Time]")))
-		return false
+		switch (false) {
+		case validateTime(self.find("#EntryTime"), self.find("input[name=Time]")):
+			return false
+		}
+		return true
 	})
 })
-var validateTime = function(e) {
+var toRFC3339 = function(d) {
+	d += ":00"
+	var t = new Date()
+	var tz = t.getTimezoneOffset()
+	if (tz > 0)
+		d += "-"
+	var h = Math.abs(parseInt(tz/60))
+	if (h < 10)
+		h = "0"+h
+	var m = tz%60
+	if (m == 0)
+		m = "00"
+	d += h+":"+m
+	return d
+}
+var validateTime = function(from, to) {
 	var fmt = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/
-	var v = e.val()
+	var v = from.val()
 	if (!fmt.test(v)) {
 		setError(e, "Invalid format. Expect: yyyy-mm-ddThh:mm")
 		return false
 	}
-	
+	to.val(toRFC3339(v))
 	return true
 }
 var setError = function(e, err) {
