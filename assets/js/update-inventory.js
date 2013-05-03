73976045bc2ecac0a5b3463ba31ecdf239ccf5c8
jake <bigjake2s@gmail.com>
2013-05-02 23:32:09 -0400

    Added PortalKeys JavaScript calculators

--- assets/js/update-inventory.js
+++ assets/js/update-inventory.js
@@ -3,59 +3,73 @@ $(function() {
 	$("#InventoryForm").submit(function() {
 		var self = $(this)
 		switch (false) {
-		case validateTime(self.find("#EntryTime"), self.find("input[name=Time]")):
+		case Inventory.validateTime(self.find("#EntryTime"), self.find("input[name=Time]")):
 			return false
 		}
-		return true
+		return false
 	})
 })
-var toRFC3339 = function(d) {
-	d += ":00"
-	var t = new Date()
-	var tz = t.getTimezoneOffset()
-	if (tz > 0)
-		d += "-"
-	var h = Math.abs(parseInt(tz/60))
-	if (h < 10)
-		h = "0"+h
-	var m = tz%60
-	if (m == 0)
-		m = "00"
-	d += h+":"+m
-	return d
-}
-var validateTime = function(from, to) {
-	var fmt = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/
-	var v = from.val()
-	if (!fmt.test(v)) {
-		setError(e, "Invalid format. Expect: yyyy-mm-ddThh:mm")
-		return false
-	}
-	to.val(toRFC3339(v))
-	return true
-}
-var setError = function(e, err) {
-	var sib
-	var sibs = e.siblings('.err')
-	if (sibs.length == 0) {
-		sib = $('<span class="err inline-help"></span>')
-		sib.insertAfter(e)
-	} else {
-		sib = $(sibs[0])
-	}
-	sib.text(err)
-	e.parents(".control-group").addClass("error")
-}
-/* Some JavaScript for Portal Keys later
-var result = 5
-try {
-	/[\d*+-/()]/
-	resut = eval("1+2")
-} catch (e) {
-	if (e instanceof SyntaxError) {
+var Inventory = {
+	PKWhitelist: /^[\d*+-\/()]+$/,
+	TimeFmt: /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/,
+	toRFC3339: function(d) {
+		d += ":00"
+		var t = new Date()
+		var tz = t.getTimezoneOffset()
+		if (tz > 0)
+			d += "-"
+		var h = Math.abs(parseInt(tz/60))
+		if (h < 10)
+			h = "0"+h
+		var m = tz%60
+		if (m == 0)
+			m = "00"
+		d += h+":"+m
+		return d
+	},
+	setError: function(e, err) {
+		var group = e.parents(".control-group")
+		var errNode = group.find('.err')
+		if (errNode.length == 0) {
+			errNode = $('<span class="err help-inline"></span>')
+			errNode.insertAfter(e)
+		}
+		errNode.text(err)
+		group.addClass("error")
+	},
+	clearError: function(e) {
+		var group = e.parents(".control-group")
+		group.find('.err').text("")
+		group.removeClass("error")
+	},
+	validateTime: function(from, to) {
+		var v = from.val()
+		if (!Inventory.TimeFmt.test(v)) {
+			Inventory.setError(from, "Invalid format. Expect: yyyy-mm-ddThh:mm")
+			return false
+		}
+		to.val(Inventory.toRFC3339(v))
+		return true
+	},
+	calculatePKs: function(from, to, disp) {
+		var v = from.val()
+		if (!Inventory.PKWhitelist.test(v)) {
+			Inventory.setError(from, "Invalid character(s). Only numbers and +, -, *, /, (, and ) allowed.")
+			return false
+		}
+		Inventory.clearError(from)
 
-	} else {
-		console.log(e.message)
+		var result
+		try {
+			eval("result = "+v)
+		} catch (e) {
+			if (e instanceof SyntaxError) {
+				// Inventory.setError(from, e.message)
+				return false
+			} else {
+				console.log(e.message)
+			}
+		}
+		return result
 	}
 }
-*/
