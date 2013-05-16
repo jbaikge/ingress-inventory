333a2fbe036d82b037f3ddc3bd8c1a995a26adbc
jake <bigjake2s@gmail.com>
2013-05-16 11:38:55 -0400

    Added mongo.AddInventory()

---
+++ mongo/add_inventory.go
@@ -0,0 +1,15 @@
+package mongo
+
+import (
+	"github.com/jbaikge/ingress-inventory/inventory"
+	"github.com/jbaikge/ingress-inventory/profile"
+	"labix.org/v2/mgo/bson"
+)
+
+func AddInventory(id interface{}, s *inventory.State) (err error) {
+	if !Connected() {
+		return ErrNotConnected
+	}
+
+	return c.Profile.UpdateId(id, bson.M{"$push": bson.M{"inventory.states": s}})
+}
